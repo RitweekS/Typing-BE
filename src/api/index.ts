08430ce0c1e30a9c36a0a45c988cc498ec7dff21
ritweek <ritweek.kumar@bootlabstech.com>
2025-03-15 13:15:51 +0530

    refactor: restructure project to use 'src' directory and update API routing

---
+++ src/api/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "../routes/authRoutes/auth";
+import testRoutes from "../routes/typingTestRoutes/typingTest";
+import leaderboardRoutes from "../routes/leaderboardRoutes/leaderboard";
+import multiplayerRoutes from "../routes/multiplayerRoutes/multiplayer";
+import friendsRoutes from "../routes/friendsRoutes/friends";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({ origin: "*", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+// Example route
+app.get("/", async (req, res) => {
+    res.send("Running");
+});
+
+app.use("/auth", authRoutes);
+app.use("/typing-tests", testRoutes);
+app.use("/leaderboard", leaderboardRoutes);
+app.use("/friends", friendsRoutes);
+
+// For local development only
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 3001;
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+// Export the Express app for Vercel
+export default app;
